0a0cca98815df0c429c1b380eb6382cd6a28e775
goscha01 <sayapingeorge@gmail.com>
2025-05-21 14:20:23 -0400

    refined the position of the scrolling

--- src/components/booking/PreferredTimes.jsx
+++ src/components/booking/PreferredTimes.jsx
@@ -203,7 +203,7 @@ export default function PreferredTimes({ form, nextStep }) {
                       </span>
                     </p>
 
-                    <div className="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-4">
+                    <div className="grid md:grid-cols-2 md:grid-cols-3 gap-1 md:gap-4">
                       {timeSlots.map((time) => (
                         <label
                           key={time}
