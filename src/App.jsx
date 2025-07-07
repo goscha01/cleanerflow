120c4375f61295c35590119baeff5eed4b32cc44
goscha01 <sayapingeorge@gmail.com>
2025-07-07 18:51:31 -0400

    fixed error with googletag

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NotFound from "@/pages/not-found";
 import Booking from "@/pages/booking";
 import Home from "@/pages/home";
 import ReactGA from 'react-ga4';
+import GoogleTag from "./GoogleTag";
 
 ReactGA.initialize('G-8W7WSSFNC6');
 
@@ -27,24 +28,11 @@ function GAListener() {
   return null;
 }
 
-useEffect(() => {
-  const script = document.createElement('script');
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-6ZB89H49SD';
-  script.async = true;
-  document.head.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { window.dataLayer.push(arguments); }
-  window.gtag = gtag;
-
-  gtag('js', new Date());
-  gtag('config', 'G-6ZB89H49SD'); // GA4
-  gtag('config', 'AW-17067419398'); // Optional: your Google Ads account ID
-}, []);
 
 function App() {
   return (
     <BrowserRouter>
+      <GoogleTag />
       <ScrollToTop />
       <GAListener /> {/* Track GA4 page views */}
       <Routes>
